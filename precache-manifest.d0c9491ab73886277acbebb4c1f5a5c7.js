520b23a92b5e1623f419cd680b7d3e4bf437a2df
varun040503 <varunreddy2439@gmail.com>
2023-06-22 00:15:27 +0530

    Updates

---
+++ precache-manifest.d0c9491ab73886277acbebb4c1f5a5c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f47aedd7976c64a99a42",
+    "url": "/colorpick/static/css/main.7eab56df.chunk.css"
+  },
+  {
+    "revision": "f47aedd7976c64a99a42",
+    "url": "/colorpick/static/js/main.f47aedd7.chunk.js"
+  },
+  {
+    "revision": "3f5be87d2a8834347623",
+    "url": "/colorpick/static/js/1.3f5be87d.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/colorpick/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "af2781497ee41105d01a",
+    "url": "/colorpick/static/js/runtime~main.af278149.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/colorpick/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "be8b2d6bd09de513903918c05c1189a3",
+    "url": "/colorpick/index.html"
+  }
+];
